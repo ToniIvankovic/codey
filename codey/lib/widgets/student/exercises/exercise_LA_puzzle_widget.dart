b787efbd726343fed42eef0565b4c80d6bcac9e2
ToniIvankovic <toni.ivankovic@gmail.com>
2024-05-03 18:18:07 +0200

    Horizontal scroll in LA code area

--- codey/lib/widgets/student/exercises/exercise_LA_puzzle_widget.dart
+++ codey/lib/widgets/student/exercises/exercise_LA_puzzle_widget.dart
@@ -37,17 +37,48 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
 
   @override
   Widget build(BuildContext context) {
+    const double spaceBetweenPiecesVertical = 8.0;
+    const double spaceBetweenPiecesHorizontal = 4.0;
     final answerPartsWithNewlines = <Widget>[];
     //add a sizedBox after each answerPart that contains a newline
+    var currentRow = <Widget>[];
     for (var i = 0; i < answerParts.length; i++) {
-      answerPartsWithNewlines.add(answerParts[i]);
+      currentRow.add(
+        Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: spaceBetweenPiecesHorizontal / 2),
+          child: answerParts[i],
+        ),
+      );
       if (answerParts[i].key.toString().contains('↵')) {
-        answerPartsWithNewlines.add(const SizedBox(
-          width: double.infinity,
-        ));
+        answerPartsWithNewlines.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                vertical: spaceBetweenPiecesVertical / 2),
+            child: Row(
+              children: List.of(currentRow),
+            ),
+          ),
+        );
+        currentRow = <Widget>[];
       }
     }
+    if (currentRow.isNotEmpty) {
+      answerPartsWithNewlines.add(
+        Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: spaceBetweenPiecesVertical / 2),
+          child: Row(
+            children: List.of(currentRow),
+          ),
+        ),
+      );
+    }
 
+    const double initialCodeAreaHeight = 150;
+    const double maxCodeAreaHeight = 400;
+    const double borderWidth = 2.0;
+    const double paddingAroundCodeArea = 8.0;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -56,7 +87,11 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-            constraints: const BoxConstraints(minHeight: 150, maxHeight: 400),
+            constraints: BoxConstraints(
+              minHeight: initialCodeAreaHeight,
+              maxHeight: maxCodeAreaHeight,
+              minWidth: MediaQuery.of(context).size.width,
+            ),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
               color: Theme.of(context).colorScheme.secondary.withOpacity(0.2),
@@ -66,20 +101,25 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
               color:
                   Theme.of(context).colorScheme.onBackground.withOpacity(0.5),
               radius: const Radius.circular(10.0),
-              strokeWidth: 2,
+              strokeWidth: borderWidth,
               dashPattern: const [6, 6],
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Wrap(
-                          spacing: 8.0, // gap between adjacent chips
-                          runSpacing: 4.0, // gap between lines
-                          children: answerPartsWithNewlines),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(paddingAroundCodeArea),
+                  child: Container(
+                    constraints: const BoxConstraints(
+                      minHeight: initialCodeAreaHeight -
+                          2 * paddingAroundCodeArea -
+                          2 * borderWidth,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: answerPartsWithNewlines,
                     ),
                   ),
-                ],
+                ),
               ),
             ),
           ),
@@ -88,6 +128,7 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: Wrap(
@@ -165,11 +206,14 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
                 ),
               ),
               //NEWLINE and TAB buttons
-              Column(
-                children: [
-                  _generateNewlineDetector(),
-                  _generateTabDetector(),
-                ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                child: Column(
+                  children: [
+                    _generateNewlineDetector(),
+                    _generateTabDetector(),
+                  ],
+                ),
               ),
             ],
           ),
@@ -205,7 +249,7 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
             ),
             child: const Padding(
               padding: EdgeInsets.all(8.0),
-              child: Text('↵'),
+              child: Text('↵', style: TextStyle(fontFamily: 'courier new')),
             ),
           ),
         );
@@ -230,7 +274,7 @@ class _ExerciseLAPuzzleWidgetState extends State<ExerciseLAPuzzleWidget> {
             padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
             child: Text(
               '↵',
-              style: TextStyle(fontSize: 20.0, fontFamily: 'monospace'),
+              style: TextStyle(fontSize: 20.0, fontFamily: 'courier new'),
             ),
           ),
         ),
