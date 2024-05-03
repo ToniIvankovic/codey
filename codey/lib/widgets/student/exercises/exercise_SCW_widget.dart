eeecd4f5bbd110922aa3f0fb218d0d3c75895b99
ToniIvankovic <toni.ivankovic@gmail.com>
2024-05-03 14:27:29 +0200

    SCW multiline code fixed

--- codey/lib/widgets/student/exercises/exercise_SCW_widget.dart
+++ codey/lib/widgets/student/exercises/exercise_SCW_widget.dart
@@ -36,69 +36,6 @@ class _ExerciseSCWWidgetState extends State<ExerciseSCWWidget> {
 
   @override
   Widget build(BuildContext context) {
-    var codeParts = widget.exercise.statementCode.split('\\gap');
-    var gaps = codeParts.length - 1;
-    Widget codeArea = Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary.withOpacity(0.2),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: DottedBorder(
-          borderType: BorderType.RRect,
-          dashPattern: const [6, 6],
-          radius: const Radius.circular(10.0),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    for (var i = 0; i < codeParts.length; i++) ...[
-                      Text(
-                        codeParts[i],
-                        style: const TextStyle(
-                          fontFamily: 'courier new',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      if (i < gaps)
-                        ConstrainedBox(
-                          constraints: BoxConstraints(
-                              minWidth:
-                                  widget.exercise.defaultGapLengths[i] * 20,
-                              maxWidth: widget.exercise.defaultGapLengths[i] *
-                                  20 *
-                                  2.5),
-                          child: IntrinsicWidth(
-                            stepWidth: 20.0,
-                            child: TextField(
-                              controller: controllers[i],
-                              onChanged: (value) {
-                                var answer =
-                                    controllers.map((e) => e.text).toList();
-                                widget.onAnswerSelected(answer);
-                              },
-                              style: const TextStyle(
-                                fontFamily: 'courier new',
-                                fontSize: 20.0,
-                              ),
-                            ),
-                          ),
-                        )
-                    ]
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-
     final statementOutputArea = widget.exercise.statementOutput != null
         ? Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -111,7 +48,8 @@ class _ExerciseSCWWidgetState extends State<ExerciseSCWWidget> {
                 widget.exercise.statementOutput!
                     .replaceAll(" ", "·")
                     .replaceAll("\t", " ⇥ "),
-                style: const TextStyle(fontSize: 18.0, fontFamily: "courier new"),
+                style:
+                    const TextStyle(fontSize: 18.0, fontFamily: "courier new"),
               ),
             ],
           )
@@ -121,7 +59,7 @@ class _ExerciseSCWWidgetState extends State<ExerciseSCWWidget> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         widget.statementArea,
-        codeArea,
+        _generateCodeArea(),
         statementOutputArea,
       ],
     );
@@ -133,5 +71,117 @@ class _ExerciseSCWWidgetState extends State<ExerciseSCWWidget> {
       controller.dispose();
     }
     super.dispose();
+  }
+
+  Widget _generateCodeArea() {
+    var textLines = widget.exercise.statementCode.split("\n");
+    var codeParts = widget.exercise.statementCode.split('\\gap');
+    var gaps = codeParts.length - 1;
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: DottedBorder(
+          borderType: BorderType.RRect,
+          dashPattern: const [6, 6],
+          radius: const Radius.circular(10.0),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 1.5,
+                minWidth: MediaQuery.of(context).size.width,
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ..._generateLinesWithGaps(textLines),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _generateLinesWithGaps(List<String> lines) {
+    int usedLines = 0;
+    List<Widget> widgets = [];
+    for (var line in lines) {
+      if (!line.contains('\\gap')) {
+        widgets.add(
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  line,
+                  style: const TextStyle(
+                    fontFamily: 'courier new',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+        continue;
+      }
+
+      var parts = line.split('\\gap');
+      var rowWidgets = <Widget>[];
+      for (var part in parts) {
+        if (part.isNotEmpty) {
+          rowWidgets.add(
+            Text(
+              part,
+              style: const TextStyle(
+                fontFamily: 'courier new',
+                fontSize: 20.0,
+              ),
+            ),
+          );
+        }
+        if (usedLines < controllers.length) {
+          rowWidgets.add(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                    minWidth: widget.exercise.defaultGapLengths[usedLines] * 18,
+                    maxWidth: widget.exercise.defaultGapLengths[usedLines] *
+                        18 *
+                        2.5),
+                child: IntrinsicWidth(
+                  stepWidth: 18.0,
+                  child: TextField(
+                    controller: controllers[usedLines],
+                    onChanged: (value) {
+                      var answer = controllers.map((e) => e.text).toList();
+                      widget.onAnswerSelected(answer);
+                    },
+                    style: const TextStyle(
+                      fontFamily: 'courier new',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+          usedLines++;
+        }
+      }
+      widgets.add(Row(children: rowWidgets));
+    }
+    return widgets;
   }
 }
