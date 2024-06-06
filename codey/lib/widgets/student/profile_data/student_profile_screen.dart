5b6acf886dec527cd6e26b71e424f39cabb78b0b
ToniIvankovic <toni.ivankovic@gmail.com>
2024-06-06 13:39:33 +0200

    Profile redesign

--- codey/lib/widgets/student/profile_data/student_profile_screen.dart
+++ codey/lib/widgets/student/profile_data/student_profile_screen.dart
@@ -44,67 +44,88 @@ class StudentProfileScreen extends StatelessWidget {
                   return Container(
                     constraints: const BoxConstraints(maxWidth: 400),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        const Text(
+                          "Moji podatci",
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        const SizedBox(height: 50),
                         const Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Expanded(
-                              child: Text("Korisničko ime:",
-                                  style: TextStyle(fontSize: 16)),
+                              child: Text(
+                                "Korisničko ime:",
+                                style: TextStyle(fontSize: 12),
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ],
                         ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(user.email,
                                 style: const TextStyle(fontSize: 16)),
                           ],
                         ),
-                        const SizedBox(height: 10.0),
+                        const SizedBox(height: 30),
                         const Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Expanded(
-                                child: Text("Ime:",
-                                    style: TextStyle(fontSize: 16))),
+                              child: Text(
+                                "Ime:",
+                                style: TextStyle(fontSize: 12),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
                           ],
                         ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(user.firstName ?? "",
                                 style: const TextStyle(fontSize: 16)),
                           ],
                         ),
-                        const SizedBox(height: 10.0),
+                        const SizedBox(height: 30),
                         const Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Expanded(child: Text("Prezime:", style: TextStyle(fontSize: 16))),
+                            Expanded(
+                              child: Text(
+                                "Prezime:",
+                                style: TextStyle(fontSize: 12),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
                           ],
                         ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(user.lastName ?? "",
                                 style: const TextStyle(fontSize: 16)),
                           ],
                         ),
-                        const SizedBox(height: 10.0),
+                        const SizedBox(height: 30),
                         if (user.dateOfBirth != null) ...[
                           const Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Expanded(
-                                child: Text("Datum rođenja:",
-                                    style: TextStyle(fontSize: 16)),
+                                child: Text(
+                                  "Datum rođenja:",
+                                  style: TextStyle(fontSize: 12),
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
                             ],
                           ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
                                   "${user.dateOfBirth!.day}.${user.dateOfBirth!.month}.${user.dateOfBirth!.year}.",
